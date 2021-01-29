4cf5a4e5d11cbdf564e832b85e0c50c3825e9146
Greg Schuman <gcschuman@hotmail.com>
2021-01-29 11:56:21 -0800

    Updated the home page to include the header and search components.

--- job-search/src/components/HomePage.js
+++ job-search/src/components/HomePage.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Header from './Header';
+import Search from './Search'
 
 const HomePage = () => {
-    return <div className="container">Job Search</div>
+    return(
+        <div>
+            <Header />
+            <Search />
+        </div>
+    )
 }
 
 export default HomePage;
